57c5a447605f169f8501a364b2ad616f482d4691
Kristin Eliassen <kristin.eliassen@neptune-software.com>
2023-09-21 15:04:17 +0200

    Fix clearing buttons and graph

--- artifacts/Application/datamodeller/event/sap.m.Button/btnClearSelectedTables/press.js
+++ artifacts/Application/datamodeller/event/sap.m.Button/btnClearSelectedTables/press.js
@@ -1,4 +1,5 @@
 const selectedTables = modeltableModeller_AllTables.getData().forEach((item) => item.selected = false);
+modeltableModeller_AllTables.refresh(true);
 tableTables.fireSelectionChange();
 butTablesToggleSelected.setPressed(false);
 butTablesToggleSelected.firePress();
--- artifacts/Application/datamodeller/event/sap.ui.table.TreeTable/treeTablePackages/rowSelectionChange.js
+++ artifacts/Application/datamodeller/event/sap.ui.table.TreeTable/treeTablePackages/rowSelectionChange.js
@@ -10,12 +10,15 @@ if (selectedRow && tableTables.getSelectedItems().length) {
 
 if (selectedRow) {
     const selectedPackage = selectedRow.getBindingContext().getObject();
-    modelSelected.oData.package = selectedPackage.id;
+    modelSelected.getData().package = selectedPackage.id;
+    modelSelected.refresh(true);
 
     const packageTableIds = selectedPackage.children.map((item) => item.id);
     if (packageTableIds.length) {
         tableModeller.displayTables(packageTableIds);
     }
 } else {
+    modelSelected.getData().package = '';
+    modelSelected.refresh(true);
     tableModeller.clearGraph();
 }
