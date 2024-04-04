6aec9afef451e46a01c524c548bb531382e50ca1
Kristin Eliassen <kristin.eliassen@neptune-software.com>
2024-04-04 10:16:41 +0200

    Make update function async

--- artifacts/Application/datamodeller/event/sap.m.Button/btnUpdate/press.js
+++ artifacts/Application/datamodeller/event/sap.m.Button/btnUpdate/press.js
@@ -1,17 +1,19 @@
-await getTableData();
+(async function () {
+    await getTableData();
 
-const selectedPackage = modelSelected.getData().package;
-if (!selectedPackage) {
-    return;
-}
-const package = modeltableModeller_AllPackages
-    .getData()
-    .find((item) => item.id === selectedPackage);
-if (!package) {
-    return;
-}
-const packageTableIds = package.tables.map((item) => item.id);
-if (!packageTableIds.length) {
-    return;
-}
-tableModeller.displayTables(packageTableIds);
+    const selectedPackage = modelSelected.getData().package;
+    if (!selectedPackage) {
+        return;
+    }
+    const package = modeltableModeller_AllPackages
+        .getData()
+        .find((item) => item.id === selectedPackage);
+    if (!package) {
+        return;
+    }
+    const packageTableIds = package.tables.map((item) => item.id);
+    if (!packageTableIds.length) {
+        return;
+    }
+    tableModeller.displayTables(packageTableIds);
+})();
